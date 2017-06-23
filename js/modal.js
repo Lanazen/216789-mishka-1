5d9dca1fffcc63d3d2f198161d599dd7beb3c2fd
Svetlana <svetatressla@gmail.com>
2017-06-24 02:30:48 +0700

    Trevis fix

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,16 @@
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
 var link = document.querySelector(".modal-cart");
-var closeModal = function () {
-	if (modal.classList.contains("modal--show")) {
-		modal.classList.remove("modal--show");
-		overlay.classList.remove("overlay--show");
-	}
-}
-link.addEventListener("click", function (event) {
-	event.preventDefault();
-	overlay.classList.add("overlay--show");
-	modal.classList.add("modal--show");
-});
-window.addEventListener("keydown", function (event) {
-	if (event.keyCode === 27) {
-		closeModal()
-	}
-});
-overlay.addEventListener("click", function(event) {
-	event.preventDefault();
-	closeModal()
-});
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        overlay.classList.add("overlay--show");
+        modal.classList.add("modal--show");
+      });
+      window.addEventListener("keydown", function (event) {
+        if (event.keyCode === 27) {
+          if (modal.classList.contains("modal--show")) {
+            modal.classList.remove("modal--show");
+            overlay.classList.remove("overlay--show");
+          }
+        }
+      });
